e33e9a336fe6ca42fda8a9b65cccb34bb2f1f8dc
Taylor Williams <taylor@Taylors-MacBook-Pro-2.local>
2021-01-07 21:17:49 -0700

    Small fixes to weather

--- src/Weather/WeatherForecastItem.js
+++ src/Weather/WeatherForecastItem.js
@@ -1,8 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import './WeatherForecastItem.css'
 
+function getImgSrc(weatherCode, timeOfDay) {
+  if (weatherCode.includes('clear') || weatherCode.includes('partly')) {
+    return `/weatherIcons/${weatherCode}_${timeOfDay}.svg`
+  }
+  return `/weatherIcons/${weatherCode}.svg`
+}
+
+/**
+ * getTimeOfDay - Returns the css depending on the time of day
+ * @param {Date} sunrise 
+ * @param {Date} sunset 
+ */
+function getTimeOfDay(sunrise, sunset) {
+  const current = new Date()
+  return (current < sunset && current > sunrise) ? 'day' : 'night'
+}
+
 export default function WeatherForecastItem({ weatherForecastItem, dayOfWeek }) {
+  const timeOfDay = getTimeOfDay(new Date(weatherForecastItem?.sunrise?.value), new Date(weatherForecastItem?.sunset?.value))
+  const imgSrc = weatherForecastItem ? getImgSrc(weatherForecastItem.weather_code.value, timeOfDay) : ''
+
   return (
     <div className="item">
       <div className='day-of-week'>
@@ -10,7 +30,7 @@ export default function WeatherForecastItem({ weatherForecastItem, dayOfWeek }) 
       </div>
       <img
         className='forecast-item-icon'
-        src={`/weatherIcons/${weatherForecastItem.weather_code.value}.svg`}
+        src={imgSrc}
         alt={weatherForecastItem.weather_code.value}
       />
       <div className='forecast-temps'>
